ef2be3b06e0e51418a247421562a0de03ab3cb4d
eguono imonieroh <imonieroheguono@gmail.com>
2025-11-24 13:04:44 +0100

    feat:hero page contents

--- src/pages/Home/components/GetStarted.jsx
+++ src/pages/Home/components/GetStarted.jsx
@@ -3,9 +3,14 @@ import React from 'react'
 const GetStarted = () => {
   return (
     <>
-    <main className='wrapper'>
+    <main className='wrapper '>
+      <section className='text-center pt-8 md:pt-24'>
 
-    <h1>GetStarted</h1>
+    <h1 className='text-[#212121] font-semibold md:font-bold text-[24px] md:text-[44px] '>Get Started</h1>
+    <p className='text-[#474747] text-[16px] md:text-[24px] font-normal pt-6 '>Whether you're applying for a passport, visa, or travel certificate, getting started is simple. Just follow these clear steps to complete your process quickly, securely, and with confidence.</p>
+      </section>
+
+
     </main>
     </>
   )
